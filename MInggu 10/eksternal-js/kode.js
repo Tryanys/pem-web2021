5e453a568e48ceb530a66f37466eb28af7b8d3c1
Trian Yuka Sulistya Jati <ryansulistyajati@gmail.com>
2021-12-20 18:33:40 +0700

    Minggu 11

--- MInggu 10/eksternal-js/kode.js
+++ MInggu 10/eksternal-js/kode.js
@@ -1,1 +1,11 @@
 alert("Hello, ini adalah program JS eksternal!");
+var ulangi = confirm("Apakah anda mau mengulang?");
+var counter = 0;
+while(ulangi){
+ counter++;
+ ulangi = confirm("Apakah anda mau mengulang?");
+}
+document.write("Perulangan sudah dilakuakn sebanyak "+ counter +" kali");
+for(let i = 0; i < 10; i++){
+    document.write("<p>Perulangan ke-" + i + "</p>")
+   }
